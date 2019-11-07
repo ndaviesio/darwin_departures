0e3fcb39f22822501ac6d7e945a33e720044f712
Nathan Davies <nathan.davies@devopsguys.com>
2019-11-07 13:38:10 +0000

    fix(Board): Fixed bug where environment station config was no longer being picked up as the default

--- src/Board.js
+++ src/Board.js
@@ -9,12 +9,38 @@ import {version} from '../package.json'
 class Board extends React.Component {
     constructor() {
         super();
+        
+        /*
+            Extract Station Identifiers to display
+            Three options are available to users:
 
-        if(window.location.search.slice(1).split('=').find(item => { return item === "crs"})){
-            let index = window.location.search.slice(1).split('=').findIndex(item => { return item === "crs"})
-            var CRS = window.location.search.slice(1).split('=')[index + 1].split(",")
+            1) Path based definition
+                This allows users to specify a comma seperated list directly in the path of the URL (e.g. /CDF,CDQ,PAD)
+            2) Query based definition
+                This allows users to specify a comma seperated list as part of a query string (e.g. ?crs=CDF,CDQ,PAD)
+            3) Environment based definition (Default)
+                Using a .env file in the project root, users can specify a comma seperated list against REACT_APP_STATIONS
+                or users can specify this in their system environment using the same name.
+
+                When the project is built using npm run build, if the REACT_APP_STATIONS variable is within the build environment,
+                the value will be compiled into the artefact.
+
+            Order of Precendance
+            1. Query Based
+            2. Path Based
+            3. .env file configuration 
+        */
+        var CRS = []
+
+        let queryCRS = window.location.search.slice(1).split('=').findIndex(item => { return item === "crs"})
+
+        if(queryCRS >= 0){
+            CRS = window.location.search.slice(1).split('=')[queryCRS + 1].split(",")
         } else {
-            CRS = decodeURI(window.location.pathname).slice(1).toUpperCase().split(",")
+            var pathCRS = decodeURI(window.location.pathname).slice(1).toUpperCase()
+            if(pathCRS !== ""){
+                CRS = pathCRS.split(",")
+            }
         }
 
         this.state = {
