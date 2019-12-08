65aaaf661d1ab6508b4aa386f8a85c9adb39dcac
Nathan Davies <me@ndavies.io>
2019-12-08 21:01:09 +0000

    feat(Snow): Added snow, cause, snow.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-import './Snow.scss'
 import Board from './Board'
 
 function App() {
--- src/Snow.js
+++ src/Snow.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Snow.scss'
 
 class Snow extends React.Component {
     constructor(props) {
